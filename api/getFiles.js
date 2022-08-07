4819ddbb3cb132daeb6ffb4a2a24c7ce49aaac06
Nisseboy <nils@tjarnlunds.se>
2022-08-07 21:29:54 +0200

    stupis

--- api/getFiles.js
+++ api/getFiles.js
@@ -20,7 +20,7 @@ const getFiles = async (uuid) => {
   const client = await pool.connect();
   try {
     let res = await client.query("select files from users where id = '" + uuid + "')");
-    return res.rows[0];
+    return res;
   } catch (err) {
     console.log(err.stack);
   } finally {
