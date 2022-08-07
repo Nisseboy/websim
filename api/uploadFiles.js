46394ea68bb6877df1024f2d54fa4aac68d719a5
Nisseboy <nils@tjarnlunds.se>
2022-08-07 18:36:42 +0200

    db test

--- api/uploadFiles.js
+++ api/uploadFiles.js
@@ -1,5 +1,31 @@
+const { Pool } = require('pg');
+let pool;
+
 async function saveFiles(req, res) {
-  return [req.body, req.cookies];
+  let token = req.cookies.token;
+  let files = req.body.files;
+
+  await insertRows();
+
+  return "jaaaas queen";
+}
+
+const insertRows = async () => {
+  if (!pool) {
+    const connectionString = process.env.DATABASE_URL
+    pool = new Pool({
+      connectionString,
+      max: 1
+    });
+  }
+  const client = await pool.connect();
+  try {
+    await client.query("INSERT INTO table (col1, col2) VALUES (val1, val2)"");
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    client.release();
+  }
 }
 
 
