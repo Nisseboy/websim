b4903fb19ad9f6d177a83fc7352fc514cc3f9a33
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2022-08-17 10:38:38 +0200

    Removed bloat and fixed wrong order removing things

--- public/index.js
+++ public/index.js
@@ -9,14 +9,6 @@ currentEditor = 0;
 
 document.addEventListener("keydown", e=>{
   if (e.ctrlKey && e.code == "KeyS") {
-    root.iterate(file=>{
-      file.code = file.tempCode;
-      file.saved = true;
-
-      if (file.openElement)
-        file.openElement.classList.remove("unsaved");
-      file.hierarchyElement.classList.remove("unsaved");
-    }, true);
     save();
     e.preventDefault();
   }
--- public/saving.js
+++ public/saving.js
@@ -1,52 +1,6 @@
 let owner = document.location.pathname.split("/")[2];
 document.cookie = `owner=${owner};path=/`;
 
-function ssave() {
-  root.iterate(file=>{
-    file.code = file.tempCode;
-    file.saved = true;
-  });
-
-  let state = {
-    root: root,
-    editors: [
-      {
-        currentFile: editors[0].currentFile,
-        opened: editors[0].opened
-      },
-      {
-        currentFile: editors[1].currentFile,
-        opened: editors[1].opened
-      },
-    ],
-    selectedFile: selectedFile
-  };
-  localStorage.setItem("state", JSON.stringify(state));
-
-  renderFiles();
-}
-
-function sload() {
-  let state = JSON.parse(localStorage.getItem("state"));
-  if (state == null) return;
-
-  root = restoreFile(state.root);
-  rectify();
-
-  selectedFile = state.selectedFile;
-
-  if (state.editors[0].currentFile)
-    changeFile(fromPath(state.editors[0].currentFile), 0);
-  editors[0].opened = state.editors[0].opened;
-
-  if (state.editors[1].currentFile)
-    changeFile(fromPath(state.editors[1].currentFile), 1);
-  editors[1].opened = state.editors[1].opened;
-
-  renderFiles();
-}
-
-
 
 async function save() {
   root.iterate(file=>{
